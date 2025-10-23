a358ab724f2bec3faf8758e3d240e49994335dde
cyborgaeon <38399164+CyborgAeon@users.noreply.github.com>
2025-10-23 12:19:33 +0100

    add edit char functionality, remove warnings from build output

--- labyItems/Pages/AddCharacter.xaml.cs
+++ labyItems/Pages/AddCharacter.xaml.cs
@@ -1,13 +1,25 @@
 using labyItems.Models;
 using labyItems.Services;
+using LiteDB;
 
 namespace labyItems.Pages;
 
 public partial class AddCharacter : ContentPage
 {
-    public AddCharacter()
+    private ObjectId _charId;
+    public AddCharacter(Character? character = null)
     {
         InitializeComponent();
+        if (character != null)
+        {
+            Title = $"Edit {character.Name}";
+            NameEntry.Text = character.Name;
+            ClassEntry.Text = character.Class;
+            PointsEntry.Text = character.Points.ToString();
+            PlayerEntry.Text = character.PlayerName;
+            _charId = character.Id;
+        }
+        BindingContext = this;
     }
 
     private async void OnSave(object sender, EventArgs e)
@@ -22,6 +34,7 @@ public partial class AddCharacter : ContentPage
 
         LiteDbService.UpsertCharacter(new Character
         {
+            Id = _charId,
             Name = NameEntry.Text!.Trim(),
             Class = ClassEntry.Text!.Trim(),
             Points = long.Parse(PointsEntry.Text!.Trim()),
