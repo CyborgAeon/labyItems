29c4bf86866387d38d07ab24846c8e9fd7dc56ca
BradBarfoot <38399164+CyborgAeon@users.noreply.github.com>
2026-01-12 08:28:56 +0000

    Moved to using a dictionary search instead of picker for big enum option sets

--- labyItems/Pages/Calculator/General/GeneralConfigPage.xaml.cs
+++ labyItems/Pages/Calculator/General/GeneralConfigPage.xaml.cs
@@ -16,24 +16,13 @@ public partial class GeneralConfigPage : ConfigPageBase<GeneralConfig>
     public GeneralConfigPage()
     {
         InitializeComponent();
-        // BindingContext = new GeneralConfig();
-        // ReturnFromConfigCommand = new Command(async () =>
-        // {
-        //     if (BindingContext is not GeneralConfig cfg)
-        //         return;
-        //     // cfg.LtmType = PowerbaseEnum.Physical;
-        //     var result = new CalcResult { TotalIsp = cfg.Total, Summary = BuildSummary(cfg) };
-
-        //     _tcs.TrySetResult(result);
-        //     await StickyFooterControl.DefaultNavigateAsync(this);
-        // });
     }
 
     private string BuildNotes(GeneralConfig c)
     {
         var s = new List<string>();
 
-    AddResistanceLevels(s, c);
+        AddResistanceLevels(s, c);
         s.AddToSummaryIf(
             c.CastingLevelsCount,
             $"+{c.CastingLevelsCount} Casting levels {(c.CastingLevelsColour?.ToString() ?? "None")}"
